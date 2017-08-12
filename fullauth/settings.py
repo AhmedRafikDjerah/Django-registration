ff245c65199e7147ca379ce7c4f7557921eee630
AhmedRafikDjerah <meinherz75@gmail.com>
2017-08-12 20:42:44 +0100

    add email auth backend

--- fullauth/settings.py
+++ fullauth/settings.py
@@ -100,7 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+AUTHENTICATION_BACKENDS = {
+    "django.contrib.auth.backends.ModelBackend",
+    "account.AuthBackends.EmailAuthBackend",
+}
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
